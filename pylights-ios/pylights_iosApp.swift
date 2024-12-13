94b381bbda3235cf291df839e5c5b9cf7e10e5f9
bguliano <72895782+bguliano@users.noreply.github.com>
2024-12-12 20:15:36 -0500

    Initial Commit

---
+++ pylights-ios/pylights_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  pylights_iosApp.swift
+//  pylights-ios
+//
+//  Created by Braden Guliano on 12/12/24.
+//
+
+import SwiftUI
+
+@main
+struct pylights_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
